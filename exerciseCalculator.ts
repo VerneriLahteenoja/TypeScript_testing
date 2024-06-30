3e2fbfcbf440184039dde2249790fb44a10fc3bb
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-06-30 19:01:10 +0300

    complete exerciseCalculator

--- exerciseCalculator.ts
+++ exerciseCalculator.ts
@@ -1,15 +1,20 @@
 
-interface calcResultObject {
+interface CalcResultObject {
   periodLength: number,
   trainingDays: number,
   success: boolean,
-  rating: string,//number
+  rating: number,
   ratingDescription: string,
-  target: string,//number
+  target: number,
   avarage: number
 }
 
-const calculateExercises = (hours: number[], target: number): calcResultObject => {
+interface Rating {
+  ratingValue: number,
+  ratingDescription: string
+}
+
+const calculateExercises = (hours: number[], target: number): CalcResultObject => {
   /*
   Return:
     number of days 
@@ -27,15 +32,31 @@ const calculateExercises = (hours: number[], target: number): calcResultObject =
     return acc + hours
   }, 0)
   const avarageHoursTrained = trainingDaysSum / days
+
+  // Rating logic
+  const ratingChoices = {
+    "bad": {ratingValue: 1, ratingDescription: "do better"},
+    "ok": {ratingValue: 2, ratingDescription: "not bad but could be better"},
+    "good": {ratingValue: 3, ratingDescription: "well done"}
+  }
+  let rating: Rating;
+  if (avarageHoursTrained < target && avarageHoursTrained >= (target * 0.75)) {
+    rating = {...ratingChoices.ok}
+  } else if (avarageHoursTrained >= target) {
+    rating = {...ratingChoices.good}
+  } else {
+    rating = {...ratingChoices.bad}
+  }
+
   return {
     periodLength: days,
     trainingDays: trainingDays.length,
     success: trainingDays.length >= target,
-    rating: 'to-be-added',
-    ratingDescription: 'to-be-added',
-    target: 'to-be-added',
+    rating: rating.ratingValue,
+    ratingDescription: rating.ratingDescription,
+    target: target,
     avarage: avarageHoursTrained
   }
 }
 
-console.log(calculateExercises([0,5,5], 2))
+console.log(calculateExercises([1,2,2], 2))
