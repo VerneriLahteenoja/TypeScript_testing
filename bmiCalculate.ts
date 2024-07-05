2d04a0013000318ce4a3da7b6a34c2cf807feb49
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-07-05 21:18:18 +0300

    refactor logging to return string instead

--- bmiCalculate.ts
+++ bmiCalculate.ts
@@ -1,27 +1,27 @@
 import { parseBmiArguments } from "./parseArguments"
 
-const calculateBmi = (args: string[]) => {
-    const {height, weight} = parseBmiArguments(args)
-    const bmi = weight / (height * 2) * 100
-    if (bmi >= 40) {
-        console.log("Obese (Class 3)")
-    } else if (bmi >= 35 && bmi <= 39.9) {
-        console.log("Obese (Class 2)")
-    } else if (bmi >= 30 && bmi <= 34.9) {
-        console.log("Obese (Class 1)")
-    } else if (bmi >= 25 && bmi <= 29.9) {
-        console.log("Overweight (Pre-obese")
-    } else if (bmi >= 18.5 && 24.9) {
-        console.log("Normal (Healthy weight)")
-    } else if (bmi >= 17 && bmi <= 18.4) {
-        console.log("Underweight (Mild thinness)")
-    } else if (bmi >= 16 && bmi <= 16.9) {
-        console.log("Underweight (Moderate thinness)")
-    } else if (bmi <= 16) {
-        console.log("Underweight (Severe thinness)")
-    } else {
-        console.log("Something went wrong...")
-    }
+const calculateBmi = (args: string[]): string => {
+  const {height, weight} = parseBmiArguments(args)
+  const bmi = weight / (height * 2) * 100
+  if (bmi >= 40) {
+    return "Obese (Class 3)"
+  } else if (bmi >= 35 && bmi <= 39.9) {
+    return "Obese (Class 2)"
+  } else if (bmi >= 30 && bmi <= 34.9) {
+    return "Obese (Class 1)"
+  } else if (bmi >= 25 && bmi <= 29.9) {
+    return "Overweight (Pre-obese)"
+  } else if (bmi >= 18.5 && 24.9) {
+    return "Normal (Healthy weight)"
+  } else if (bmi >= 17 && bmi <= 18.4) {
+    return "Underweight (Mild thinness)"
+  } else if (bmi >= 16 && bmi <= 16.9) {
+    return "Underweight (Moderate thinness)"
+  } else if (bmi <= 16) {
+    return "Underweight (Severe thinness)"
+  } else {
+    return "Something went wrong..."
+  }
 }
 
-calculateBmi(process.argv)
+console.log(calculateBmi(process.argv))
