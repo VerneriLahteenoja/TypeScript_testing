ad873b67029d9928ceeb3de51eb5a372dcbd59aa
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-07-06 22:38:23 +0300

    create an endpoint for bmi that accepts query parameters height and weight

--- index.ts
+++ index.ts
@@ -1,10 +1,26 @@
 import express from 'express'
+import qs from 'qs'
+import { calculateBmi } from './bmiCalculate';
 
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
 });
+
+app.get('/bmi', (req, res) => {
+  var params = qs.parse(req.query as any)
+  if (!params.height || !params.weight || Object.keys(params).length !== 2) {
+    return res.status(400).json({ error: 'Malformatted parameters. Expected height=<number>&weight=<number>'})
+  }
+  const height = params.height as string
+  const weight = params.weight as string
+
+  const bmi = calculateBmi([height, weight])
+  console.log(bmi)
+  
+  return res.json({ bmi })
+})
 
 const PORT = 3003;
 
