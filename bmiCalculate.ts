f1e7d641282f7c0d928e64f5747f3ee65a58958f
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-07-09 00:01:51 +0300

    add semicolons where necessary

--- bmiCalculate.ts
+++ bmiCalculate.ts
@@ -1,27 +1,27 @@
-import { parseBmiArguments } from "./parseArguments"
+import { parseBmiArguments } from "./parseArguments";
 
 export const calculateBmi = (args: string[]): string => {
-  const {height, weight} = parseBmiArguments(args)
-  const bmi = weight / (height * 2) * 100
+  const {height, weight} = parseBmiArguments(args);
+  const bmi = weight / (height * 2) * 100;
   if (bmi >= 40) {
-    return "Obese (Class 3)"
+    return "Obese (Class 3)";
   } else if (bmi >= 35 && bmi <= 39.9) {
-    return "Obese (Class 2)"
+    return "Obese (Class 2)";
   } else if (bmi >= 30 && bmi <= 34.9) {
-    return "Obese (Class 1)"
+    return "Obese (Class 1)";
   } else if (bmi >= 25 && bmi <= 29.9) {
-    return "Overweight (Pre-obese)"
+    return "Overweight (Pre-obese)";
   } else if (bmi >= 18.5 && 24.9) {
-    return "Normal (Healthy weight)"
+    return "Normal (Healthy weight)";
   } else if (bmi >= 17 && bmi <= 18.4) {
-    return "Underweight (Mild thinness)"
+    return "Underweight (Mild thinness)";
   } else if (bmi >= 16 && bmi <= 16.9) {
-    return "Underweight (Moderate thinness)"
+    return "Underweight (Moderate thinness)";
   } else if (bmi <= 16) {
-    return "Underweight (Severe thinness)"
+    return "Underweight (Severe thinness)";
   } else {
-    return "Something went wrong..."
+    return "Something went wrong...";
   }
-}
+};
 
 //console.log(calculateBmi(process.argv))
